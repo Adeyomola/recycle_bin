0e3b639b32c8eec052e3a599623c87ebcb3645fb
Adeyomola <kayzeel@yahoo.com>
2022-09-19 21:53:54 +0100

    Added autodelete function

--- trash.sh
+++ trash.sh
@@ -16,7 +16,15 @@ else
 ( sudo mkdir /trash/.metadata; sudo chmod o+w /trash/.metadata ) #creates metadata directory if it doesn't exist
 fi
 
-#define positional arguments
+#checks if metadata_count directory exists
+if [[ -d /trash/.metadata_count ]]
+then
+:
+else
+( sudo mkdir /trash/.metadata_count; sudo chmod o+w /trash/.metadata_count ) #creates writeable metadata_count directory
+fi
+
+#define positional arguments and variables
 arg1=$1 #argument 1 - indicates whether you intend to delete or restore a file
 arg2=$2 #argument 2 - indiates the file
 arg3=$( sudo ls -i $arg2 | awk '{print $1}' ) #argument 3 - the file's inode
@@ -25,6 +33,7 @@ wd=$( pwd ) #working directory/the file's directory of origin
 #delete function
 function delete() {
 ( sudo echo $wd > /trash/.metadata/${arg3} )
+( sudo echo 0 > /trash/.metadata_count/${arg3} )
 ( sudo mv -i "$arg2" /trash )
 }
 
@@ -32,6 +41,38 @@ function delete() {
 function restore() {
 ( sudo mv -i "$arg2" $( cat /trash/.metadata/${arg3} ) )
 ( sudo rm /trash/.metadata/${arg3} )
+( sudo rm /trash/.metadata_count/${arg3} )
+}
+
+#autodelete after 30days
+function autodelete() {
+for i in /trash/.metadata_count/*
+do
+
+if [[ -z $i ]]
+then
+:
+elif [[ $( cat $i ) == 31 ]]
+then
+
+arg4=$( sudo ls -i | grep ${i##*/} |awk '{print $2}' ) #retrieves filename using inode
+
+( sudo rm /trash/${arg4} )
+( sudo rm "${i}" )
+fi
+done
+}
+
+#counter
+function counter() {
+for i in /trash/.metadata_count/*
+do
+#echo $i
+#echo $( cat $i )
+echo $(( $( cat ${i} ) + 1 )) > ${i}
+done
+
+autodelete
 }
 
 #case switch
@@ -40,5 +81,7 @@ case $arg1 in
 delete;;
 "-r")
 restore;;
+"-c")
+counter;;
 esac
 
