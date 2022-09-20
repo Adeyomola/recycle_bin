a5511cef3b031534a3a72cd46c40e3294f4e6cfa
Adeyomola <kayzeel@yahoo.com>
2022-09-20 08:55:10 +0100

    Fixed error with metadata_count directory

--- trash.sh
+++ trash.sh
@@ -24,6 +24,7 @@ else
 ( sudo mkdir /trash/.metadata_count; sudo chmod o+w /trash/.metadata_count ) #creates writeable metadata_count directory
 fi
 
+
 #define positional arguments and variables
 arg1=$1 #argument 1 - indicates whether you intend to delete or restore a file
 arg2=$2 #argument 2 - indiates the file
@@ -33,7 +34,7 @@ wd=$( pwd ) #working directory/the file's directory of origin
 #delete function
 function delete() {
 ( sudo echo $wd > /trash/.metadata/${arg3} )
-( sudo echo 0 > /trash/.metadata_count/${arg3} )
+( sudo echo 1 > /trash/.metadata_count/${arg3} )
 ( sudo mv -i "$arg2" /trash )
 }
 
@@ -44,35 +45,25 @@ function restore() {
 ( sudo rm /trash/.metadata_count/${arg3} )
 }
 
-#autodelete after 30days
-function autodelete() {
+#counter
+function counter() {
+if [[ $( ls -A /trash/.metadata_count ) ]] #checks if the directory is not empty
+then
 for i in /trash/.metadata_count/*
 do
-
-if [[ -z $i ]]
-then
-:
-elif [[ $( cat $i ) == 31 ]]
-then
+echo $(( $( cat $i ) + 1 )) > $i
 
 arg4=$( sudo ls -i | grep ${i##*/} |awk '{print $2}' ) #retrieves filename using inode
 
+if [[ $( cat $i ) == 31 ]] #deletes the files after 30 days
+then
 ( sudo rm /trash/${arg4} )
 ( sudo rm "${i}" )
 fi
 done
-}
-
-#counter
-function counter() {
-for i in /trash/.metadata_count/*
-do
-#echo $i
-#echo $( cat $i )
-echo $(( $( cat ${i} ) + 1 )) > ${i}
-done
-
-autodelete
+else #if the directory is empty, do nothing
+:
+fi
 }
 
 #case switch
